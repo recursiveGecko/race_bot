fca15f1866ad9f486aa34e21d2401f35722e4219
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2022-09-14 19:51:11 +0200

    add basic analytics

--- lib/f1_bot_web/views/layout_view.ex
+++ lib/f1_bot_web/views/layout_view.ex
@@ -4,4 +4,8 @@ defmodule F1BotWeb.LayoutView do
   # Phoenix LiveDashboard is available only in development by default,
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
+
+  def phx_host() do
+    F1Bot.get_env(F1BotWeb.Endpoint)[:url][:host]
+  end
 end
