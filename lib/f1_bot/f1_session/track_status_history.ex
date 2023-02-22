e73d92153baf0d173f1327953ecb206c17c28cc4
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2023-02-22 23:32:20 +0100

    fix tests

--- lib/f1_bot/f1_session/track_status_history.ex
+++ lib/f1_bot/f1_session/track_status_history.ex
@@ -78,7 +78,7 @@ defmodule F1Bot.F1Session.TrackStatusHistory do
           ts_from: (if interval.starts_at, do: DateTime.to_unix(interval.starts_at)),
           ts_to: (if interval.ends_at, do: DateTime.to_unix(interval.ends_at)),
           # Add margin to the start and end of the interval to make it visually clearer which laps were affected
-          lap_from: (if interval.status == :red_flag, do: interval.start_lap - 0.25),
+          lap_from: (if interval.status == :red_flag and interval.start_lap, do: interval.start_lap - 0.25),
           lap_to: (if interval.end_lap, do: interval.end_lap + 0.25),
           status: humanize_status(interval),
           type:
