45be67111b4f30ee9741e50fa133fd6f70bf2c27
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2023-03-02 13:54:51 +0100

    limit height of driver stint displays with scrollable overflow

--- lib/f1_bot_web/components/driver_summary.ex
+++ lib/f1_bot_web/components/driver_summary.ex
@@ -160,133 +160,137 @@ defmodule F1BotWeb.Component.DriverSummary do
 
       <p class="text-left text-sm mb-1 text-gray-500">Stint information</p>
 
-      {#for stint <- @driver_summary.stints}
-        <div class="mt-1 p-0.5 bg-white drop-shadow rounded-lg">
-          <div class="flex flex-wrap items-center">
-            <span class="mr-auto pl-0.5 pr-2">
-              <Component.TyreSymbol
-                class="h-6 inline-block"
-                tyre_age={stint.tyre_age}
-                compound={stint.compound}
-              />
-            </span>
+      <div class="max-h-64 overflow-auto flex flex-col-reverse">
+        <div>
+          {#for stint <- @driver_summary.stints}
+            <div class="mt-1 p-0.5 bg-white drop-shadow rounded-lg">
+              <div class="flex flex-wrap items-center">
+                <span class="mr-auto pl-0.5 pr-2">
+                  <Component.TyreSymbol
+                    class="h-6 inline-block"
+                    tyre_age={stint.tyre_age}
+                    compound={stint.compound}
+                  />
+                </span>
 
-            <span
-              class="pr-2 sm:basis-auto text-sm text-gray-500"
-              title="Stint start time (UTC)"
-              :if={stint.start_time != nil}
-            >
-              {Timex.format!(stint.start_time, "{h24}:{m}")} UTC
-            </span>
-            <span class="pr-2 sm:basis-auto text-sm text-gray-500" title="Stint start/end (lap numbers)">
-              Laps: {stint.lap_start}-{stint.lap_end}
-            </span>
-            <span
-              class="pr-2 sm:basis-auto text-sm text-gray-500"
-              title="Number of laps included in statistics (excl. outlaps, inlaps, VSC, SC)"
-            >
-              Timed laps: {stint.timed_laps}
-            </span>
-          </div>
+                <span
+                  class="pr-2 sm:basis-auto text-sm text-gray-500"
+                  title="Stint start time (UTC)"
+                  :if={stint.start_time != nil}
+                >
+                  {Timex.format!(stint.start_time, "{h24}:{m}")} UTC
+                </span>
+                <span class="pr-2 sm:basis-auto text-sm text-gray-500" title="Stint start/end (lap numbers)">
+                  Laps: {stint.lap_start}-{stint.lap_end}
+                </span>
+                <span
+                  class="pr-2 sm:basis-auto text-sm text-gray-500"
+                  title="Number of laps included in statistics (excl. outlaps, inlaps, VSC, SC)"
+                >
+                  Timed laps: {stint.timed_laps}
+                </span>
+              </div>
 
-          <div class="mt-0.5 grid grid-cols-[minmax(max-content,1.3fr)_repeat(3,1fr)] sm:grid-cols-4 gap-0.5">
-            <div class="contents">
-              <span class="text-sm" title="Fastest lap">
-                <span class="block sm:hidden">&nbsp;</span>
-                <span class="px-1 inline-block w-7">FL</span>
-                <Component.LapTimeField
-                  id={generate_field_id(@driver_info, stint, :fastest_lap)}
-                  class="px-0.5 rounded border"
-                  stats={stint.stats}
-                  personal_best_stats={@driver_summary.stats}
-                  session_best_stats={@session_best_stats}
-                  kind={:fastest_lap}
-                />
-              </span>
-              <span class="text-sm" title="Fastest sector 1">
-                <span class="px-1 block sm:inline-block">S1</span>
-                <Component.LapTimeField
-                  id={generate_field_id(@driver_info, stint, :fastest_sector_1)}
-                  class="px-0.5 rounded border"
-                  stats={stint.stats}
-                  personal_best_stats={@driver_summary.stats}
-                  session_best_stats={@session_best_stats}
-                  kind={{:fastest_sector, 1}}
-                />
-              </span>
-              <span class="text-sm" title="Fastest sector 2">
-                <span class="px-1 block sm:inline-block">S2</span>
-                <Component.LapTimeField
-                  id={generate_field_id(@driver_info, stint, :fastest_sector_2)}
-                  class="px-0.5 rounded border"
-                  stats={stint.stats}
-                  personal_best_stats={@driver_summary.stats}
-                  session_best_stats={@session_best_stats}
-                  kind={{:fastest_sector, 2}}
-                />
-              </span>
-              <span class="text-sm" title="Fastest sector 3">
-                <span class="px-1 block sm:inline-block">S3</span>
-                <Component.LapTimeField
-                  id={generate_field_id(@driver_info, stint, :fastest_sector_3)}
-                  class="px-0.5 rounded border"
-                  stats={stint.stats}
-                  personal_best_stats={@driver_summary.stats}
-                  session_best_stats={@session_best_stats}
-                  kind={{:fastest_sector, 3}}
-                />
-              </span>
+              <div class="mt-0.5 grid grid-cols-[minmax(max-content,5.3fr)_repeat(3,1fr)] sm:grid-cols-4 gap-0.5">
+                <div class="contents">
+                  <span class="text-sm" title="Fastest lap">
+                    <span class="block sm:hidden">&nbsp;</span>
+                    <span class="px-1 inline-block w-7">FL</span>
+                    <Component.LapTimeField
+                      id={generate_field_id(@driver_info, stint, :fastest_lap)}
+                      class="px-0.5 rounded border"
+                      stats={stint.stats}
+                      personal_best_stats={@driver_summary.stats}
+                      session_best_stats={@session_best_stats}
+                      kind={:fastest_lap}
+                    />
+                  </span>
+                  <span class="text-sm" title="Fastest sector 1">
+                    <span class="px-1 block sm:inline-block">S1</span>
+                    <Component.LapTimeField
+                      id={generate_field_id(@driver_info, stint, :fastest_sector_1)}
+                      class="px-0.5 rounded border"
+                      stats={stint.stats}
+                      personal_best_stats={@driver_summary.stats}
+                      session_best_stats={@session_best_stats}
+                      kind={{:fastest_sector, 1}}
+                    />
+                  </span>
+                  <span class="text-sm" title="Fastest sector 2">
+                    <span class="px-1 block sm:inline-block">S2</span>
+                    <Component.LapTimeField
+                      id={generate_field_id(@driver_info, stint, :fastest_sector_2)}
+                      class="px-0.5 rounded border"
+                      stats={stint.stats}
+                      personal_best_stats={@driver_summary.stats}
+                      session_best_stats={@session_best_stats}
+                      kind={{:fastest_sector, 2}}
+                    />
+                  </span>
+                  <span class="text-sm" title="Fastest sector 3">
+                    <span class="px-1 block sm:inline-block">S3</span>
+                    <Component.LapTimeField
+                      id={generate_field_id(@driver_info, stint, :fastest_sector_3)}
+                      class="px-0.5 rounded border"
+                      stats={stint.stats}
+                      personal_best_stats={@driver_summary.stats}
+                      session_best_stats={@session_best_stats}
+                      kind={{:fastest_sector, 3}}
+                    />
+                  </span>
+                </div>
+
+                <div class="contents text-gray-500">
+                  <span class="text-sm" title="Average lap">
+                    <span class="px-1 w-7 inline-block">Avg.</span>
+                    <Component.LapTimeField
+                      id={generate_field_id(@driver_info, stint, :average_lap)}
+                      class="px-0.5 rounded border"
+                      stats={stint.stats}
+                      personal_best_stats={@driver_summary.stats}
+                      session_best_stats={@session_best_stats}
+                      kind={:average_lap}
+                    />
+                  </span>
+                  <span class="text-sm" title="Average sector 1">
+                    <span class="px-1 hidden sm:inline-block">S1</span>
+                    <Component.LapTimeField
+                      id={generate_field_id(@driver_info, stint, :average_sector_1)}
+                      class="px-0.5 rounded border"
+                      stats={stint.stats}
+                      personal_best_stats={@driver_summary.stats}
+                      session_best_stats={@session_best_stats}
+                      kind={{:average_sector, 1}}
+                    />
+                  </span>
+                  <span class="text-sm" title="Average sector 2">
+                    <span class="px-1 hidden sm:inline-block">S2</span>
+                    <Component.LapTimeField
+                      id={generate_field_id(@driver_info, stint, :average_sector_2)}
+                      class="px-0.5 rounded border"
+                      stats={stint.stats}
+                      personal_best_stats={@driver_summary.stats}
+                      session_best_stats={@session_best_stats}
+                      kind={{:average_sector, 2}}
+                    />
+                  </span>
+                  <span class="text-sm" title="Average sector 3">
+                    <span class="px-1 hidden sm:inline-block">S3</span>
+                    <Component.LapTimeField
+                      id={generate_field_id(@driver_info, stint, :average_sector_3)}
+                      class="px-0.5 rounded border"
+                      stats={stint.stats}
+                      personal_best_stats={@driver_summary.stats}
+                      session_best_stats={@session_best_stats}
+                      kind={{:average_sector, 3}}
+                    />
+                  </span>
+                </div>
+              </div>
             </div>
-
-            <div class="contents text-gray-500">
-              <span class="text-sm" title="Average lap">
-                <span class="px-1 w-7 inline-block">Avg.</span>
-                <Component.LapTimeField
-                  id={generate_field_id(@driver_info, stint, :average_lap)}
-                  class="px-0.5 rounded border"
-                  stats={stint.stats}
-                  personal_best_stats={@driver_summary.stats}
-                  session_best_stats={@session_best_stats}
-                  kind={:average_lap}
-                />
-              </span>
-              <span class="text-sm" title="Average sector 1">
-                <span class="px-1 hidden sm:inline-block">S1</span>
-                <Component.LapTimeField
-                  id={generate_field_id(@driver_info, stint, :average_sector_1)}
-                  class="px-0.5 rounded border"
-                  stats={stint.stats}
-                  personal_best_stats={@driver_summary.stats}
-                  session_best_stats={@session_best_stats}
-                  kind={{:average_sector, 1}}
-                />
-              </span>
-              <span class="text-sm" title="Average sector 2">
-                <span class="px-1 hidden sm:inline-block">S2</span>
-                <Component.LapTimeField
-                  id={generate_field_id(@driver_info, stint, :average_sector_2)}
-                  class="px-0.5 rounded border"
-                  stats={stint.stats}
-                  personal_best_stats={@driver_summary.stats}
-                  session_best_stats={@session_best_stats}
-                  kind={{:average_sector, 2}}
-                />
-              </span>
-              <span class="text-sm" title="Average sector 3">
-                <span class="px-1 hidden sm:inline-block">S3</span>
-                <Component.LapTimeField
-                  id={generate_field_id(@driver_info, stint, :average_sector_3)}
-                  class="px-0.5 rounded border"
-                  stats={stint.stats}
-                  personal_best_stats={@driver_summary.stats}
-                  session_best_stats={@session_best_stats}
-                  kind={{:average_sector, 3}}
-                />
-              </span>
-            </div>
-          </div>
+          {/for}
         </div>
-      {/for}
+      </div>
     </div>
     """
   end
