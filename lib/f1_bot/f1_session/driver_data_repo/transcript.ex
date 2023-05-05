18fc06b1150524264e7348a20df92a54cfea5536
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2023-05-05 02:16:23 +0200

    tweaks

--- lib/f1_bot/f1_session/driver_data_repo/transcript.ex
+++ lib/f1_bot/f1_session/driver_data_repo/transcript.ex
@@ -7,21 +7,43 @@ defmodule F1Bot.F1Session.DriverDataRepo.Transcript do
   @primary_key false
 
   embedded_schema do
-    field :driver_number, :integer
-    field :duration_sec, :float
-    field :utc_date, :utc_datetime
-    field :message, :string
+    field(:driver_number, :integer)
+    field(:duration_sec, :float)
+    field(:utc_date, :utc_datetime)
+    field(:estimated_real_date, :utc_datetime)
+    field(:playhead_utc_date, :utc_datetime)
+    field(:message, :string)
   end
 
   def validate(params) do
     %__MODULE__{}
-    |> cast(params, [:driver_number, :utc_date, :message, :duration_sec])
-    |> validate_required([:driver_number, :utc_date, :message, :duration_sec])
+    |> cast(params, [
+      :driver_number,
+      :utc_date,
+      :playhead_utc_date,
+      :estimated_real_date,
+      :message,
+      :duration_sec
+    ])
+    |> validate_required([
+      :driver_number,
+      :utc_date,
+      :message,
+      :duration_sec
+    ])
     |> apply_action(:validate)
   end
 
   def to_event(this = %__MODULE__{}) do
-    ts = DateTime.to_unix(this.utc_date, :millisecond)
+    date =
+      if this.estimated_real_date != nil do
+        this.estimated_real_date
+      else
+        recording_date = this.utc_date || this.playhead_utc_date || DateTime.utc_now()
+        DateTime.add(recording_date, -20, :second)
+      end
+
+    ts = DateTime.to_unix(date, :millisecond)
     Event.new("driver:transcript", %{transcript: this}, ts)
   end
 end
--- lib/f1_bot_web/channels/transcriber_service_channel.ex
+++ lib/f1_bot_web/channels/transcriber_service_channel.ex
@@ -44,7 +44,7 @@ defmodule F1BotWeb.TranscriberServiceChannel do
   end
 
   defp process_transcript(transcript = %Transcript{}, _socket) do
-    Logger.info("Received transcript: #{inspect(transcript)}")
+    Logger.debug("Received transcript: #{inspect(transcript)}")
     Server.process_transcript(transcript)
 
     broadcast_to_topics = [
