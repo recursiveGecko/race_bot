8699e4a2796bf2ca44a0f2fae7c4f951f761c0a9
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2023-11-16 12:33:29 +0100

    fix: discord command mode config option

--- config/runtime.exs
+++ config/runtime.exs
@@ -54,7 +54,7 @@ if config_env() == :prod do
     config :f1_bot,
       connect_to_signalr: true,
       start_discord: true,
-      discord_command_type: :global,
+      discord_command_mode: :global,
       discord_api_module: F1Bot.ExternalApi.Discord.Live,
       twitter_api_module: F1Bot.ExternalApi.Twitter.Live
 
