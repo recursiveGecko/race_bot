5bf5f4d15cb9fba68247141a14da9f42d816de87
recursiveGecko <1461520+recursiveGecko@users.noreply.github.com>
2024-06-27 11:52:54 +0200

    bump version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule F1Bot.MixProject do
   def project do
     [
       app: :f1_bot,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       source_url: "https://github.com/recursiveGecko/race_bot",
